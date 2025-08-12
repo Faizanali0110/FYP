ebb5035fff5f9e5057b5c645fe5723c8b2eef3c0
Salah-ud-din Sani <salahuddinsani10@gmail.com>
2025-08-12 11:57:01 +0500

    error solved in LOGIN

--- Fyp/src/pages/Login.jsx
+++ Fyp/src/pages/Login.jsx
@@ -120,7 +120,7 @@ export function Login() {
                                     required
                                 />
                             </div>
-{/*hello
+
                             <div className="relative">
                                 <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
                                     <FaLock className="h-5 w-5 text-gray-400" />
@@ -197,4 +197,3 @@ export function Login() {
         </>
     );
 }
-*/}
